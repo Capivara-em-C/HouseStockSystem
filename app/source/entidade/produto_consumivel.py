352e88032b9e90cb1400c058191550fc6104491b
Gilmar Bec <gilmarmscontato@gmail.com>
2020-10-30 18:57:43 -0300

    Add entidades estoque e produto consumivel

---
+++ app/source/entidade/produto_consumivel.py
@@ -0,0 +1,37 @@
+from app.source.entidade.produto_abstrato import ProdutoAbstrato
+from app.source.entidade.estoque import Estoque
+from app.source.helpers.setter import validacao_tipo
+
+
+class ProdutoConsumivel(ProdutoAbstrato):
+    def __init__(
+            self,
+            identificador: int,
+            nome: str = "",
+            descricao: str = "",
+            data_fabricacao: str = "",
+            categorias: dict = None,
+            ultimo_valor: float = 0,
+            prioridade: int = 0,
+            estoque: Estoque = None
+    ):
+        super().__init__(
+            identificador,
+            nome,
+            descricao,
+            data_fabricacao,
+            categorias,
+            ultimo_valor,
+            prioridade
+        )
+
+        self.estoque = estoque
+
+    @property
+    def estoque(self):
+        return self.__estoque
+
+    @estoque.setter
+    def estoque(self, estoque: Estoque):
+        validacao_tipo(estoque, Estoque)
+        self.__estoque = estoque
